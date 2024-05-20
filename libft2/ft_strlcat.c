40a9b028d9798dfde05e62da19db583961ebd3be
Dan <77102436+Dan-dino@users.noreply.github.com>
2024-05-20 12:56:31 +0400

    Add files via upload

--- libft2/ft_strlcat.c
+++ libft2/ft_strlcat.c
@@ -6,11 +6,12 @@
 /*   By: daalhosa <daalhosa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 10:21:26 by daalhosa          #+#    #+#             */
-/*   Updated: 2024/05/18 13:29:52 by daalhosa         ###   ########.fr       */
+/*   Updated: 2024/05/20 12:51:45 by daalhosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -21,10 +22,17 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	j = 0;
 	if (dstsize > 0)
 	{
-		while (++j < dstsize && src[j] != '\0')
-		{
+		while (++j < dstsize || src[j] != '\0')
 			dst[i + j] = src[j];
-		}
 	}
+	dst[j] = '\0';
 	return (j);
+}
+
+int	main(void)
+{
+	char	str[] = "abcdefg";
+	char	str2[] = "hijklmnop";
+	strlcat(str2, str, 8);
+	printf("%s", str);
 }
--- libft2/ft_strlcpy.c
+++ libft2/ft_strlcpy.c
@@ -6,46 +6,33 @@
 /*   By: daalhosa <daalhosa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 11:54:52 by daalhosa          #+#    #+#             */
-/*   Updated: 2024/05/18 13:06:11 by daalhosa         ###   ########.fr       */
+/*   Updated: 2024/05/20 11:35:43 by daalhosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
-// {
-// 	size_t	i;
-
-// 	i = ft_strlen(src);
-// 	if (dstsize > 0)
-// 	{
-// 		dst[dstsize] = '\0';
-// 		dstsize--;
-// 		while (dstsize && src[dstsize] != '\0')
-// 		{
-// 			dst[dstsize] = src[dstsize];
-// 			dstsize--;
-// 		}
-// 	}
-// 	return (i);
-// }
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	len;
 
-	i = 0;
+	i = -1;
 	len = ft_strlen(src);
-	if (!dstsize)
-		return (len);
+	if (dstsize > 0)
+	{
+		while (++i < dstsize && src[i] != '\0')
+			dst[i] = src[i];
+		dst[i] = '\0';
+	}
+	return (len);
 }
 
 int	main(void)
 {
-	char	dst[] = "this is a string";
-	char	src[] = "another string";
+	char	dst[] = "abcdefgh";
+	char	src[] = "hijklmnop";
 
-	ft_strlcpy(dst, src, 9);
+	ft_strlcpy(dst, src, 8);
 	printf("%s\n", dst);
 }
