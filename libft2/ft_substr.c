c8a8a17c9fb98ee168688e95542391fd5e1994e8
Dan <77102436+Dan-dino@users.noreply.github.com>
2024-06-01 17:16:41 +0400

    Update ft_substr.c

--- libft2/ft_substr.c
+++ libft2/ft_substr.c
@@ -7,7 +7,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub = malloc(sizeof (char) * len + 1);
 	if (!sub)
 		return (NULL);
-	ft_strlcpy(sub, s + start, len +1);
+	ft_strlcpy(sub, s + start, len);
 	sub[len] = '\0';
 	return (sub);
 }
